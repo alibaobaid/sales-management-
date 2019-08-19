2738999fc08d28d10f99ddccea3ccdc545b1842f
ali amrah <ali_amrah@jisr.net>
2019-08-19 06:52:20 +0300

    change nill value

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Manger.create(name:"مدير المنشاءة")
-User.create(username:"admin", password:"admin", role_type:'مدير', is_default: true)
-User.create(username:"user", password:"user", role_type:'مستخدم', is_default: false)
+# Manger.create(name:"مدير المنشاءة")
+# User.create(username:"admin", password:"admin", role_type:'مدير', is_default: true)
+# User.create(username:"user", password:"user", role_type:'مستخدم', is_default: false)
+Marketer.all.map{|m| m.update_columns(for_him: 0) if m.for_him.nil? }
+Delegate.all.map do |delegate|
+  delegate.update_columns(for_him:0)if delegate.for_him.nil?
+  delegate.update_columns(amount_of_box: 0) if delegate.amount_of_box.nil?
+  delegate.update_columns(amount_of_gallon: 0)if delegate.amount_of_gallon.nil?
+end
