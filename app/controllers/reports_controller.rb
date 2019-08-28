19b0dd59b7122522eab920e56320add919cf2a9f
Ali Amrah <ali.b5556789@gmail.com>
2019-08-28 17:09:45 +0300

    enhance report and pagination

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,12 +2,23 @@ class ReportsController < ApplicationController
   skip_before_action :authrized
   def sales_report
     @sales_operations = \
-      if params[:search].present?
-        SalesOperation.where(date: params[:search] )
+      if params[:from].present? && params[:to].present? && params[:marketer_id].try(:[],:id).present? && params[:delegate_id].try(:[],:id).present?
+        @current_country.sales_operations.between(params[:from], params[:to]).delegate(params[:delegate_id][:id]).marketer(params[:marketer_id][:id])
+      elsif params[:from].present? && params[:to].present? && params[:delegate_id].try(:[],:id).present?
+        @current_country.sales_operations.between(params[:from], params[:to]).delegate(params[:delegate_id][:id])
+      elsif params[:from].present? && params[:to].present? && params[:marketer_id].try(:[],:id).present?
+        @current_country.sales_operations.between(params[:from], params[:to]).marketer(params[:marketer_id][:id])
+      elsif params[:marketer_id].try(:[],:id).present? && params[:delegate_id].try(:[],:id).present?
+        @current_country.sales_operations.where(date: Date.today).delegate(params[:delegate_id][:id]).marketer(params[:marketer_id][:id])
+      elsif params[:delegate_id].try(:[],:id).present?
+        @current_country.sales_operations.where(date: Date.today).delegate(params[:delegate_id][:id])
+      elsif params[:marketer_id].try(:[],:id).present?
+        @current_country.sales_operations.where(date: Date.today).marketer(params[:marketer_id][:id])
+      elsif params[:from].present? && params[:to].present?
+        @current_country.sales_operations.between(params[:from], params[:to])
       else
-        SalesOperation.where(date: Date.today)
+        @current_country.sales_operations.where(date: Date.today)
       end
-
     respond_to do |format|
       format.html
       format.xlsx
