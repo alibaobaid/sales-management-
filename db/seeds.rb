925e7c6b0065aebb3f0850ef63345d835df4111c
Ali Amrah <ali.b5556789@gmail.com>
2019-09-01 16:11:23 +0300

    fix build error

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Country.create(name: 'السعوديه')
-Country.create(name: 'الامارات')
-Country.create(name: 'البحرين')
-Country.create(name: 'عمان')
-Country.create(name: 'الكويت')
-Country.create(name: 'اليمن')
-Country.create(name: 'الاردن')
-Country.create(name: 'قطر')
-Country.create(name: 'مصر')
-Country.create(name: 'العراق')
+# Country.create(name: 'السعوديه')
+# Country.create(name: 'الامارات')
+# Country.create(name: 'البحرين')
+# Country.create(name: 'عمان')
+# Country.create(name: 'الكويت')
+# Country.create(name: 'اليمن')
+# Country.create(name: 'الاردن')
+# Country.create(name: 'قطر')
+# Country.create(name: 'مصر')
+# Country.create(name: 'العراق')
 
-Country.all.each do |country|
-  Manger.create(name:"مدير المنشاءة", country_id: country.id)
-  User.create(username:"admin", password:"admin", role_type:'مدير', is_default: true, country_id: country.id)
-  User.create(username:"user", password:"user", role_type:'مستخدم', is_default: false, country_id: country.id)
-end
+# Country.all.each do |country|
+#   Manger.create(name:"مدير المنشاءة", country_id: country.id)
+#   User.create(username:"admin", password:"admin", role_type:'مدير', is_default: true, country_id: country.id)
+#   User.create(username:"user", password:"user", role_type:'مستخدم', is_default: false, country_id: country.id)
+# end
 # Delegate.all.map{|delegate| delegate.update(country_id: 1) if delegate.country_id.nil? }
 # Marketer.all.map{|marketer| marketer.update(country_id: 1) if marketer.country_id.nil? }
 # Assistant.all.map{|assistant| assistant.update(country_id: 1) if assistant.country_id.nil? }
